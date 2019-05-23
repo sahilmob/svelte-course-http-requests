ada1888777921a9d1310c59b1747dcc764a4b3d7
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-23 16:15:13 +0300

    send basic POST request

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,36 @@
 <script>
-	export let name;
+  import { FIREBASE_BASE_URL } from "./constants";
+  let hobbies = [];
+  let hobbyInput;
+
+  function addHobby() {
+    hobbies = [...hobbies, hobbyInput.value];
+    fetch(`${FIREBASE_BASE_URL}/hobbies.json`, {
+      method: "POST",
+      body: JSON.stringify(hobbies),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("falid");
+        }
+      })
+      .catch(err => console.log("err"));
+  }
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+
 </style>
 
-<h1>Hello {name}!</h1>
+<label for="hobby">Hobby</label>
+<input bind:this={hobbyInput} type="text" id="hobby" />
+<button on:click={addHobby}>Add Hobby</button>
+
+<ul>
+  {#each hobbies as hobby (hobby)}
+    <li>{hobby}</li>
+  {/each}
+</ul>
